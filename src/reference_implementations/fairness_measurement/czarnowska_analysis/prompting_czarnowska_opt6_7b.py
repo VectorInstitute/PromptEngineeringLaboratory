a09a452722d8fa2db9c04bb4e2c1ebfc0787d1fc
David Emerson <43939939+emersodb@users.noreply.github.com>
2024-06-06 09:20:27 -0400

    Additional edits to the prompting scripts to allow for bash scripting orchestration. Added slurm files.

--- src/reference_implementations/fairness_measurement/czarnowska_analysis/prompting_czarnowska_opt6_7b.py
+++ src/reference_implementations/fairness_measurement/czarnowska_analysis/prompting_czarnowska_opt6_7b.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import random
 import re
@@ -23,20 +24,13 @@ OutputEntry = Tuple[
     PredictedLabel, TrueLabel, Category, Group, TestText, Model, RunID, Dataset, NumParams,
 ]
 
-RUN_ID = "run_1"  # run_1, run_2, run_3, run_4, run_5
-DATASET = "SST5"  # Labeled task-specific dataset, SST5, SemEval, or ZeroShot
-
 PATH_STUB = "src/reference_implementations/fairness_measurement/resources"
 TEST_FILE_PATH = f"{PATH_STUB}/czarnowska_templates/sentiment_fairness_tests.tsv"
-MODEL_PATH = "/ssd005/projects/llm/opt-6.7b"
 MODEL = "OPT-6.7B"
 NUM_PARAMS: float = 6.7  # billions
 BATCH_SIZE = 4
 N_SHOTS = 9
-# Append results to this file.
-PREDICTION_FILE_PATH = (
-    f"{PATH_STUB}/prompt_tuning_fairness_paper_preds/opt6_7b/opt_6_7b_prompt_predictions_{DATASET}_{RUN_ID}.tsv"
-)
+
 SEEDS = {
     "run_1": 2024,
     "run_2": 2025,
@@ -44,12 +38,6 @@ SEEDS = {
     "run_4": 2027,
     "run_5": 2028,
 }
-
-# Setting random seed according to run ID
-SEED = SEEDS[RUN_ID]
-np.random.seed(SEED)
-random.seed(SEED)
-torch.manual_seed(SEED)
 
 MODEL_PATH = "/ssd005/projects/llm/opt-6.7b"
 generator = pipeline("text-generation", model=MODEL_PATH, device="cuda")
@@ -67,12 +55,12 @@ number_of_demonstrations_per_label = N_SHOTS // 3
 number_of_random_demonstrations = N_SHOTS - number_of_demonstrations_per_label * 3
 
 
-def create_demonstrations() -> str:
-    if DATASET == "SST5":
+def create_demonstrations(dataset: str) -> str:
+    if dataset == "SST5":
         path = "src/reference_implementations/fairness_measurement/czarnowska_analysis/resources/processed_sst5.tsv"
     else:
         path = "src/reference_implementations/fairness_measurement/czarnowska_analysis/resources/processed_semeval.tsv"
-    if DATASET != "ZeroShot":
+    if dataset != "ZeroShot":
         df = pd.read_csv(path, sep="\t", header=0)
         # Trying to balance the number of labels represented in the demonstrations
         sample_df_negative = df.Valence[df.Valence.eq("Negative")].sample(number_of_demonstrations_per_label).index
@@ -99,17 +87,17 @@ def create_demonstrations() -> str:
         return ""
 
 
-def create_prompt_for_text(text: str, demonstrations: str) -> str:
-    if DATASET != "ZeroShot":
+def create_prompt_for_text(text: str, demonstrations: str, dataset: str) -> str:
+    if dataset != "ZeroShot":
         return f"{demonstrations}Text: {text}\nQuestion: What is the sentiment of the text?\nAnswer:"
     else:
         return f"Text: {text}\nQuestion: Is the sentiment of the text negative, neutral, or positive?\nAnswer:"
 
 
-def create_prompts_for_batch(input_texts: List[str], demonstrations: str) -> List[str]:
+def create_prompts_for_batch(input_texts: List[str], demonstrations: str, dataset: str) -> List[str]:
     prompts = []
     for input_text in input_texts:
-        prompts.append(create_prompt_for_text(input_text, demonstrations))
+        prompts.append(create_prompt_for_text(input_text, demonstrations), dataset)
     return prompts
 
 
@@ -123,9 +111,9 @@ def extract_predicted_label(sequence: str) -> str:
         return random.choice(["positive", "negative", "neutral"])
 
 
-def get_predictions_batched(input_texts: List[str], demonstrations: str) -> List[str]:
+def get_predictions_batched(input_texts: List[str], demonstrations: str, dataset: str) -> List[str]:
     predicted_labels = []
-    prompts = create_prompts_for_batch(input_texts, demonstrations)
+    prompts = create_prompts_for_batch(input_texts, demonstrations, dataset)
     batched_sequences = generator(prompts, do_sample=True, max_new_tokens=2, temperature=0.8, return_full_text=False)
     for prompt_sequence in batched_sequences:
         generated_text = prompt_sequence[0]["generated_text"]
@@ -137,56 +125,84 @@ def get_predictions_batched(input_texts: List[str], demonstrations: str) -> List
     return predicted_labels
 
 
-tests: List[TestEntry] = []
-with open(TEST_FILE_PATH, "r") as template_file:
-    for line in template_file.readlines():
-        label_str, attribute, group, text = tuple(line.rstrip().split("\t"))
-        # convert the label string to an int
-        label = int(label_str)
-        tests.append((label, attribute, group, text))
-
-test_batches = [tests[x : x + BATCH_SIZE] for x in range(0, len(tests), BATCH_SIZE)]
-demonstrations = create_demonstrations()
-example_prompt = create_prompt_for_text("I did not like that movie at all.", demonstrations)
-print(f"Example Prompt\n{example_prompt}")
-
-# If the prediction file doesn't exist, we create a new one and append the tsv header row.
-if not os.path.exists(PREDICTION_FILE_PATH):
-    header_row = "\t".join(
-        ["y_true", "y_pred", "category", "group", "text", "model", "run_id", "dataset", "num_params",]
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="OPT6.7 Czarnowska Prompting")
+    parser.add_argument("--run_id", action="store", type=str, help="Should be one of run_1...run_5", default="run_1")
+    parser.add_argument(
+        "--dataset",
+        action="store",
+        type=str,
+        help="Labeled task-specific dataset to use for few-shots. Should be one of SST5, SemEval, or ZeroShot",
+        default="SST5",
     )
-    header_row = header_row + "\n"
-    with open(PREDICTION_FILE_PATH, "w") as prediction_file:
-        prediction_file.write(header_row)
+    args = parser.parse_args()
 
-text_batch: List[str] = []
-# Append to the output file instead of overwriting.
-with open(PREDICTION_FILE_PATH, "a") as prediction_file:
-    for batch in tqdm(test_batches):
-        output: List[OutputEntry] = []
-        text_batch = [test_case[-1] for test_case in batch]  # Extract texts from the batch.
-        predictions = get_predictions_batched(text_batch, demonstrations)
+    run_id = args.run_id
+    dataset = args.dataset
+    assert run_id in ["run_1", "run_2", "run_3", "run_4", "run_5"]
+    assert dataset in ["SST5", "SemEval", "ZeroShot"]
 
-        for prediction, test_entry in zip(predictions, batch):
-            label, attribute, group, text = test_entry
-            output_entry = (
-                label,
-                label_lookup[prediction],
-                attribute,
-                group,
-                text,
-                MODEL,
-                RUN_ID,
-                DATASET,
-                NUM_PARAMS,
-            )
-            output.append(output_entry)
+    # Append results to this file.
+    PREDICTION_FILE_PATH = (
+        f"{PATH_STUB}/prompt_tuning_fairness_paper_preds/opt6_7b/opt_6_7b_prompt_predictions_{dataset}_{run_id}.tsv"
+    )
 
-        output_lines = []
-        for output_entry in output:
-            output_lines.append(
-                "\t".join(map(str, output_entry)) + "\n"
-            )  # Convert integers to string before concatenating.
+    # Setting random seed according to run ID
+    SEED = SEEDS[run_id]
+    np.random.seed(SEED)
+    random.seed(SEED)
+    torch.manual_seed(SEED)
 
-        prediction_file.writelines(output_lines)
-        prediction_file.flush()
+    tests: List[TestEntry] = []
+    with open(TEST_FILE_PATH, "r") as template_file:
+        for line in template_file.readlines():
+            label_str, attribute, group, text = tuple(line.rstrip().split("\t"))
+            # convert the label string to an int
+            label = int(label_str)
+            tests.append((label, attribute, group, text))
+
+    test_batches = [tests[x : x + BATCH_SIZE] for x in range(0, len(tests), BATCH_SIZE)]
+    demonstrations = create_demonstrations(dataset)
+    example_prompt = create_prompt_for_text("I did not like that movie at all.", demonstrations, dataset)
+    print(f"Example Prompt\n{example_prompt}")
+
+    # If the prediction file doesn't exist, we create a new one and append the tsv header row.
+    if not os.path.exists(PREDICTION_FILE_PATH):
+        header_row = "\t".join(
+            ["y_true", "y_pred", "category", "group", "text", "model", "run_id", "dataset", "num_params",]
+        )
+        header_row = header_row + "\n"
+        with open(PREDICTION_FILE_PATH, "w") as prediction_file:
+            prediction_file.write(header_row)
+
+    text_batch: List[str] = []
+    # Append to the output file instead of overwriting.
+    with open(PREDICTION_FILE_PATH, "a") as prediction_file:
+        for batch in tqdm(test_batches):
+            output: List[OutputEntry] = []
+            text_batch = [test_case[-1] for test_case in batch]  # Extract texts from the batch.
+            predictions = get_predictions_batched(text_batch, demonstrations, dataset)
+
+            for prediction, test_entry in zip(predictions, batch):
+                label, attribute, group, text = test_entry
+                output_entry = (
+                    label,
+                    label_lookup[prediction],
+                    attribute,
+                    group,
+                    text,
+                    MODEL,
+                    run_id,
+                    dataset,
+                    NUM_PARAMS,
+                )
+                output.append(output_entry)
+
+            output_lines = []
+            for output_entry in output:
+                output_lines.append(
+                    "\t".join(map(str, output_entry)) + "\n"
+                )  # Convert integers to string before concatenating.
+
+            prediction_file.writelines(output_lines)
+            prediction_file.flush()
